bba2ad4a89d024fa595b0c5f64b62a01b9cdb429
Sarah Walters <swalters4925@gmail.com>
2017-02-06 03:25:14 -0500

    FIX: documenting & passing -ansi -pedantic -Wall

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,12 +1,30 @@
+/*
+   Questions:
+   - is it kosher to use the word "string" in C documentation?
+     I know they're really char arrays, but "string" is a useful
+     abstraction of a word.
+   - what naming conventions do you prefer we use, if any?
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
-void readInput(char *prompt, char *format, char *buffer) {
+/* Reads command line input into a buffer.
+   *prompt: pointer to the input prompt string
+   *format: pointer to the input formatting string
+   *buffer: pointer to the array which holds the input
+*/
+void read_input(char *prompt, char *format, char *buffer) {
     puts(prompt);
     scanf(format, buffer);
 }
 
-int getValue(char *card_name) {
+/* Gets the value associated with a card name
+   *card_name points to a string representing card name
+   returns an integer representing the value of the card
+*/
+int get_card_value(char *card_name) {
     int val = 0;
     switch (card_name[0]) {
         case 'K':
@@ -28,22 +46,29 @@ int getValue(char *card_name) {
     return val;
 }
 
-void updateCount(int val, int *count) {
-    if ((val > 2) && (val < 7)) {
+/* Updates a card count based on card value
+   card_value represents the integer value of a card
+   *count is a pointer to the counter the function updates
+*/
+void update_card_count(int card_value, int *count) {
+    if ((card_value > 2) && (card_value < 7)) {
         *count = *count + 1;
-    } else if (val == 10) {
+    } else if (card_value == 10) {
         *count = *count - 1;
     }
     printf("Current count: %i\n", *count);
 }
 
+/* Performs card counting upon user input
+*/
 int main() {
-    char card_name[3]; // buffer for user input
-    int count = 0;
+    char card_name[3]; /* buffer which is reused for user input */
+    int val; /* reused to store card values */
+    int count = 0; /* used to track card count */
     do {
-        readInput("Enter the card name: ", "%2s", card_name);
-        int val = getValue(card_name);
-        updateCount(val, &count);
+        read_input("Enter the card name: ", "%2s", card_name);
+        val = get_card_value(card_name);
+        update_card_count(val, &count);
     } while (card_name[0] != 'X');
     return 0;
 }
